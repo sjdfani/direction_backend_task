5fb4a9df98f69f90fc4df2d296e15420bc92a5ba
Sajjad fani <faniam321@gmail.com>
2024-05-29 23:52:50 +0330

    create api for scrape site

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import ExtractJobs
 
 app_name = "job"
 
 urlpatterns = [
-
+    path("extract/", ExtractJobs.as_view(), name="ExtractJobs"),
 ]
--- job/views.py
+++ job/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .tasks import automatic_extract_jobs
 
-# Create your views here.
+
+class ExtractJobs(APIView):
+    def get(self, request):
+        automatic_extract_jobs.delay()
+        message = {"message": "Job extraction started"}
+        return Response(message, status=status.HTTP_200_OK)

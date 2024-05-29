fcea3d5601582df833e9c9bc6774dc4b492f5667
Sajjad fani <faniam321@gmail.com>
2024-05-29 15:34:21 +0330

    add celery

---
+++ direction_backend/celery.py
@@ -0,0 +1,11 @@
+import os
+from celery import Celery
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'direction_backend.settings')
+
+app = Celery('direction_backend')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
